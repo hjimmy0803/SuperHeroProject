1f63c04ae3097aa3ef72c510e5448c459aabb6d7
unknown <hjimmy0803@gmail.com>
2020-02-14 16:22:12 -0600

    I added migration and named it my superheroproject and also did and update.

---
+++ MySuperHeroProject/Data/Migrations/20200214221841_MySuperHeroProject.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MySuperHeroProject.Data.Migrations
+{
+    public partial class MySuperHeroProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "superheroes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    alterEgo = table.Column<string>(nullable: true),
+                    ability = table.Column<string>(nullable: true),
+                    secondaryAbility = table.Column<string>(nullable: true),
+                    catchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_superheroes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "superheroes");
+        }
+    }
+}
