e0fdffe27c4fed9ccf4c89b92dd2e9895bb40190
unknown <hjimmy0803@gmail.com>
2020-02-14 14:36:57 -0600

    Uploading project.

---
+++ MySuperHeroProject/Models/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySuperHeroProject.Models
+{
+    public class SuperHero
+    {
+    }
+}
